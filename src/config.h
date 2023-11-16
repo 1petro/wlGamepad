ce2bf0ba41f0fb2924ae1c6045e37c538c52dc5e
1petro <petronazeh@gmail.com>
2023-11-16 14:00:23 +0000

    gui: Add new theme PlayPad for wlgp
    Using libpng16 now instead of bmp for reading image
    new Support alpha format now for allow image tansperncy
    meson update new changes

--- src/config.h
+++ src/config.h
@@ -17,10 +17,12 @@
 #define MAX_BUF_SIZE 120
 #define DEFAULT_KEYS 10
 #define DEFAULT_PROP 6
+#define DEFAULT_SIZE 50
 #define MAX_DEFAULT_BUTTONS 12
 
 //int keyparse(char **data,char *button[],Gamepad *gp,int count,int ptr);
 int getconfig(Gamepad gp[],char *config_name);
-void adj_scale(Gamepad gp[],int scale,int begin,int end);
+void adj_scale(Gamepad gp[],int sel_theme,int scale,int begin,int end);
+extern int offset_val;
 //int getoptions(struct wlkb_in *data,int argc,char *argv[]);
 #endif
